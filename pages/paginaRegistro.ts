b6bc1046e4366ba5d3c486f33f95e6f586dfbb58
Gaston <gpazdumit@gmail.com>
2026-05-06 15:58:31 -0300

    Se agrega el constructor de paginaRegistro

--- pages/paginaRegistro.ts
+++ pages/paginaRegistro.ts
@@ -15,4 +15,26 @@ export class PaginaRegistro {
   readonly buttonShowPassword: Locator;
   readonly buttonShowConfirmPassword: Locator;
   readonly buttonIrAIniciarSesion: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.inputNombre = page.getByRole('textbox', { name: 'Nombre' });
+    this.inputApellido = page.getByRole('textbox', { name: 'Apellido' });
+    this.inputEmail = page.getByRole('textbox', { name: 'Correo electrónico' });
+    this.inputPassword = page.getByRole('textbox', { name: 'Contraseña', exact: true });
+    this.inputConfirmPassword = page.getByRole('textbox', { name: 'Confirmar contraseña' });
+    this.botonRegistrarse = page.getByRole('button', { name: 'Crear cuenta' });
+    this.linkYaTengoCuenta = page.getByRole('link', { name: '¿Ya tienes cuenta? Inicia' });
+    this.checkboxTerminos = page.getByRole('checkbox', { name: 'Acepto los Términos y' });
+    this.linkTerminos = page.getByRole('link', { name: 'Términos y Condiciones' });
+    this.linkPoliticaPrivacidad = page.getByRole('link', {
+      name: 'Política de Privacidad',
+      exact: true,
+    });
+    this.buttonShowPassword = page.getByRole('button', { name: 'Mostrar contraseña' }).first();
+    this.buttonShowConfirmPassword = page
+      .getByRole('button', { name: 'Mostrar contraseña' })
+      .last();
+    this.buttonIrAIniciarSesion = page.getByRole('button', { name: 'Ir a iniciar sesión' });
+  }
 }
